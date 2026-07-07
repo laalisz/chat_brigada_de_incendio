6a97c1fc7a87c9671c5b23d2ff95002a7884bdbc
laalisz <laiessa.9@live.com>
2026-07-07 20:52:19 -0300

    atualizacao correcao e ajuste de url

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ const mensagem = document.getElementById("mensagem");
 const btnEnviar = document.getElementById("btnEnviar");
 const btnLimpar = document.getElementById("btnLimpar");
 
-const API_URL = "https://chat-brigada-de-incendio.onrender.com/";
+const API_URL = "https://chat-brigada-de-incendio.onrender.com/chat";
 
 const mensagemInicial =
   "Olá! Sou um assistente especializado em Brigada de Incêndio em Empresas. Posso ajudar com dúvidas sobre prevenção, evacuação, extintores, classes de incêndio e procedimentos básicos de emergência.";
